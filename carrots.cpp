2e35f1351d9c33e72fe1e1b48dd23867e7f0a7d3
cbroys <cbroys@svsu.edu>
2017-08-09 07:34:02 -0400

    Add files via upload

---
+++ carrots.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(){
+    
+    int num1, num2;
+    std::string word1, word2;
+    
+    std::cin >> num1 >> num2;
+    std::cin >> word1 >> word2;
+    std::cout << num2;
+    
+    return 0;
+}
